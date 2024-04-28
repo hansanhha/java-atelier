608eb0dbe61fd1c30ecf547a8eae458188aafbc7
hansanhha <kwixs35@gmail.com>
2024-04-28 13:02:12 +0900

    [project] test 모듈 추가

---
+++ test/src/test/java/com/hansannha/spring/TestApplicationTest.java
@@ -0,0 +1,14 @@
+package com.hansannha.spring;
+
+import com.hansanhha.spring.test.TestApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TestApplication.class)
+public class TestApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
