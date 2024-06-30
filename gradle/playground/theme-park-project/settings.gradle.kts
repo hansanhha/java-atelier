b549eca3b631c585d0bb50683d69bb2530ff145a
hansanhha <kwixs35@gmail.com>
2024-06-30 12:00:23 +0900

    [gradle] playground 디렉토리 개편

--- gradle/playground/theme-park-project/settings.gradle.kts
+++ gradle/playground/theme-park-project/settings.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "playground"
+rootProject.name = "theme-park-project"
 include("theme-park-status", "theme-park-api")
